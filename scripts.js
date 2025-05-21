7823c792c04b09730cf373634ee990cb20f6eb24
Larry Enrique Dias Costa <larryenriquecostadias@gmail.com>
2025-05-21 15:31:18 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded"), () => {
-    const cards = [
+    const cards = [ //imagem das cartas
     {
         name: 'windows',
         id: 'images/windows.png'
